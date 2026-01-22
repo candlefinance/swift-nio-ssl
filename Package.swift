c9979e4df6f23f3024e333828d15ebda5da52995
Nikolai Wotton <wottonn@gmail.com>
2026-01-22 00:24:26 -0500

    Pointing at branches

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ import class Foundation.ProcessInfo
 func generateDependencies() -> [Package.Dependency] {
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         return [
-            .package(url: "https://github.com/candlefinance/swift-nio.git", exact: "2.82.1-candle")
+            .package(url: "https://github.com/candlefinance/swift-nio.git", branch: "fix-candle-2.82.1")
         ]
     } else {
         return [
