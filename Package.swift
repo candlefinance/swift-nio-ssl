ee7abf544646aeb04ad360687ed10951497805a8
Nikolai Wotton <wottonn@gmail.com>
2026-01-22 10:48:03 -0500

    partial reversion

--- Package.swift
+++ Package.swift
@@ -36,11 +36,11 @@ import class Foundation.ProcessInfo
 func generateDependencies() -> [Package.Dependency] {
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         return [
-            .package(name: "candle-swift-nio", url: "https://github.com/candlefinance/candle-swift-nio.git", branch: "fix-candle-2.82.1")
+            .package(url: "https://github.com/candlefinance/candle-swift-nio.git", branch: "fix-candle-2.82.1")
         ]
     } else {
         return [
-            .package(name: "candle-swift-nio", path: "../swift-nio")
+            .package(path: "../swift-nio")
         ]
     }
 }
